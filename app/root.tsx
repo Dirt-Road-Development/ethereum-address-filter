bedcd1781126b11d850793a84ce05fd3f9579d2e
TheGreatAxios <sawyer@dirtroad.dev>
2023-02-11 14:49:03 -0800

    Inital Setup Complete

--- app/root.tsx
+++ app/root.tsx
@@ -7,12 +7,21 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import Layout from "./components/Layout";
+
+import varsCSS from "~/styles/vars.css";
+import indexCSS from "~/styles/index.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Ethereum Address Filter",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links = () => [
+  { rel: "stylesheet", href: varsCSS },
+  { rel: "stylesheet", href: indexCSS }, 
+]
 
 export default function App() {
   return (
@@ -22,7 +31,9 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <Layout>
+          <Outlet />
+        </Layout>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
